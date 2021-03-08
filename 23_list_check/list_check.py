88cd6ee3abf84a37ed295dd9b143fe22a2710254
Andrew Buckingham <abuckingham99@gmail.com>
2021-03-07 23:16:05 -0800

    Complete exercise 23

--- 23_list_check/list_check.py
+++ 23_list_check/list_check.py
@@ -7,3 +7,17 @@ def list_check(lst):
         >>> list_check([[1], "nope"])
         False
     """
+
+    for element in lst:
+        if not isinstance(element, list):
+            return False
+
+    return True
+    
+
+        # SB Alternative:
+        # Alternate possibilities: use all() with a generator comprehension,
+        # though that isn't something we've covered yet:
+        #
+        # return all(isinstance(item, list) for item in lst)
+
