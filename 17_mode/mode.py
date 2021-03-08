ac1a4a7ddbbdf8a6beebd4f19e9b03411ec9fd42
Andrew Buckingham <abuckingham99@gmail.com>
2021-03-07 21:02:46 -0800

    Complete exercise 17

--- 17_mode/mode.py
+++ 17_mode/mode.py
@@ -11,3 +11,15 @@ def mode(nums):
         >>> mode([2, 2, 3, 3, 2])
         2
     """
+
+    # I had to lean on the solution for this one. I started by trying to create a dictionary lookup, but kept getting lost in the weeds. This is kind of the concept I had in mind, so it made sense once I saw it...
+    
+    counts = {}
+
+    for num in nums:
+        counts[num] = counts.get(num, 0) + 1
+
+        max_value = max(counts.values())
+        for (num, freq) in counts.items():
+            if freq == max_value:
+                return num
