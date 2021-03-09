6fbe452778921ef17e048b544acfda4da53a5fd3
Andrew Buckingham <abuckingham99@gmail.com>
2021-03-09 13:57:07 -0800

    Commit exercise 37

--- 37_sum_up_diagonals/sum_up_diagonals.py
+++ 37_sum_up_diagonals/sum_up_diagonals.py
@@ -18,3 +18,14 @@ def sum_up_diagonals(matrix):
         >>> sum_up_diagonals(m2)
         30
     """
+    # I have to admit, I struggled with this one. I found some solutions that used the numby library, but I wasn't able to coe up with a good solution. I need to brush up on my matrix algebra...
+
+    # SB approach
+    total = 0
+    
+    for i in range(len(matrix)):
+        total += matrix[i][i]
+        total += matrix[i][-1 - i]
+        
+    return total
+    
